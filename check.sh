47bfa28b4c248edd47de825625fa6acf7c36f479
Kelv1nYu <ykq1312@126.com>
2022-05-04 15:49:24 -0400

    update templates

--- check.sh
+++ check.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+git log
 
-changeFiles=$(git diff --name-only HEAD~ HEAD)
-
+# changeFiles=$(git diff --name-only HEAD~ HEAD)
+changeFiles=$(git log --name-only)
 echo $changeFiles
 
 # aws s3 cp ./source/Fraud_Detection.ipynb s3://test-asc-sagemaker-fraud/source/Fraud_Detection_${VERSION}.ipynb
