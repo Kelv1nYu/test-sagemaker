2ad7b397b897515439a1840566b444d71c5d17fd
Kelv1nYu <ykq1312@126.com>
2022-05-05 13:45:24 -0400

    update templates

--- check.sh
+++ check.sh
@@ -6,8 +6,11 @@ if [[ $(aws s3 ls s3://test-asc-sagemaker-fraud/source/ | grep 'Fraud_Detection'
     file1="./source/Fraud_Detection.ipynb"
     file2=$(find . -name 'Fraud_Detection_*')
     if cmp -s "$file1" "$file2"; then
-        echo "two files are same"
-        exit 0;
+        echo "two files are same";
+        IFS='. ' read -r -a array <<< $OBJECT
+        IFS='_ ' read -r -a array2 <<< ${array[0]}
+        export VERSION="${array2[@]: -1:1}"
+        exit 1;
     else
         echo "two files are different"
         aws s3 cp ./source/Fraud_Detection.ipynb s3://test-asc-sagemaker-fraud/source/Fraud_Detection_${VERSION}.ipynb
