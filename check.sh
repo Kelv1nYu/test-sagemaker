0a581001ab6f38c86bb85e2fd70b28a6e97d7ff7
Kelv1nYu <ykq1312@126.com>
2022-05-05 14:00:54 -0400

    update check.sh

--- check.sh
+++ check.sh
@@ -10,7 +10,7 @@ if [[ $(aws s3 ls s3://test-asc-sagemaker-fraud/source/ | grep 'Fraud_Detection'
         IFS='. ' read -r -a array <<< $OBJECT
         IFS='_ ' read -r -a array2 <<< ${array[0]}
         export VERSION="${array2[@]: -1:1}"
-        exit 1;
+        exit 0;
     else
         echo "two files are different"
         aws s3 cp ./source/Fraud_Detection.ipynb s3://test-asc-sagemaker-fraud/source/Fraud_Detection_${VERSION}.ipynb
