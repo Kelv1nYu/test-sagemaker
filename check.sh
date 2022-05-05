c5b363625039ded1cf11568a5376226c5c5e6514
Kelv1nYu <ykq1312@126.com>
2022-05-05 12:26:17 -0400

    upadte templates

--- check.sh
+++ check.sh
@@ -2,12 +2,14 @@
 
 if [[ $(aws s3 ls s3://test-asc-sagemaker-fraud/source/ | grep 'Fraud_Detection') ]]; then 
     OBJECT="$(aws s3 ls s3://test-asc-sagemaker-fraud/source/ --recursive | sort | tail -n 1 | awk '{print $4}')"
-    aws s3 cp s3://test-asc-sagemaker-fraud/$OBJECT ./source/$OBJECT
+    aws s3 cp s3://test-asc-sagemaker-fraud/source/$OBJECT ./source/$OBJECT
     file1="./source/Fraud_Detection.ipynb"
     file2=$(find . -name 'Fraud_Detection_*')
     if cmp -s "$file1" "$file2"; then
+        echo "two files are same"
         exit 0;
     else
+        echo "two files are different"
         aws s3 cp ./source/Fraud_Detection.ipynb s3://test-asc-sagemaker-fraud/source/Fraud_Detection_${VERSION}.ipynb
     fi
 else 
