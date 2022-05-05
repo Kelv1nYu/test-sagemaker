5cddabea5d291ec9d58fdb4a7ca34bf87394e514
Kelv1nYu <ykq1312@126.com>
2022-05-05 12:04:19 -0400

    update check script

--- check.sh
+++ check.sh
@@ -3,15 +3,14 @@
 if [[ $(aws s3 ls s3://test-asc-sagemaker-fraud/source/ | grep 'Fraud_Detection') ]]; then 
     OBJECT="$(aws s3 ls s3://test-asc-sagemaker-fraud/source/ --recursive | sort | tail -n 1 | awk '{print $4}')"
     aws s3 cp s3://test-asc-sagemaker-fraud/$OBJECT ./source/$OBJECT
+    file1="./source/Fraud_Detection.ipynb"
+    file2=$(find . -name 'Fraud_Detection_*')
+    if cmp -s "$file1" "$file2"; then
+        exit 1;
+    else
+        aws s3 cp ./source/Fraud_Detection.ipynb s3://test-asc-sagemaker-fraud/source/Fraud_Detection_${VERSION}.ipynb
+    fi
 else 
-    exit 1;
-fi
-
-file1="./source/Fraud_Detection.ipynb"
-file2=$(find . -name 'Fraud_Detection_*')
-
-if cmp -s "$file1" "$file2"; then
-    exit 1;
-else
     aws s3 cp ./source/Fraud_Detection.ipynb s3://test-asc-sagemaker-fraud/source/Fraud_Detection_${VERSION}.ipynb
 fi
+
